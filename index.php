3a055c9e3d8c7ff5150e1153ab6fd70ad477c524
Elmona <emil_larsson83@hotmail.com>
2018-09-14 08:21:19 +0200

    Using  instead

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
 
 // Include the files needed.
-require_once 'Secrets.php';
 require_once 'view/LoginView.php';
 require_once 'view/DateTimeView.php';
 require_once 'view/LayoutView.php';
+require_once 'Secrets.php';
 
-// Show errors turn off in production.
 error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+ini_set('display_errors', $_ENV['display_errors']);
 
 // Create objects of the views.
 $v = new view\LoginView();
 $dtv = new view\DateTimeView();
 $lv = new view\LayoutView();
 
+// Router
 if ($_SERVER['REQUEST_METHOD'] == 'POST'
     && $_POST['LoginView::UserName']
     && $_POST['LoginView::Password']) {
-    // echo "Should try to check if user name and password is correct.";
     $lv->render(true, $v, $dtv);
 } else {
     $lv->render(false, $v, $dtv);
